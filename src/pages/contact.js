ac14afd84b8ff7428605aa5643797c1e0c9cd23c
Albert Chung <chung972@gmail.com>
2019-07-22 18:28:01 -0500

    creates 3 anchor tags

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,17 @@ const ContactPage = () => {
     return(
         <div>
             <h1>Contact</h1>
-            <p>Details</p>
+            <ul>
+                <li>
+                    <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google</a>
+                </li>
+                <li>
+                    <a href="https://www.keyhero.com/" target="_blank" rel="noopener noreferrer">KeyHero</a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">YouTube</a>
+                </li>
+            </ul>
         </div>
     )
 }
