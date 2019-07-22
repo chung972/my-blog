790a3fd81698049a499ae5e4882db9fb4df97bd2
Albert Chung <chung972@gmail.com>
2019-07-22 18:03:55 -0500

    creates and exports about functional component

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>I am a developer now</p>
+        </div>
+    )
+}
+
+export default AboutPage;
