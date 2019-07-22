5d55089f96b89df0d4d1b7e449ef575f8aaa785c
Albert Chung <chung972@gmail.com>
2019-07-22 18:03:36 -0500

    exports IndexPage functional component; adds comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return(
+        <div>
+            <h1>Hello</h1>
+            <h2>My name is Albert. A full-stack developer living in Texas.</h2>
+        </div>
+    )
+}
+
+export default IndexPage;
+// when it comes to PAGE CREATION, the src/pages/ directory
+// is where gatsby looks when it's figuring out what STATIC pages
+// your site needs; all the files in the pages directory will be
+// the pages for our site
