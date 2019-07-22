6396b2f77d63359373603a33a918ca7adf165919
Albert Chung <chung972@gmail.com>
2019-07-22 18:28:26 -0500

    adds an anchor tag and Link component; adds comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React from "react";
+import {Link} from "gatsby";
 
 const IndexPage = () => {
     return(
         <div>
             <h1>Hello</h1>
             <h2>My name is Albert. A full-stack developer living in Texas.</h2>
+            <p>Need a developer? <a href="/contact">Contact me.</a></p>
+            {/* if you're linking to an EXTERNAL site, you can use an <a> just fine,
+            but for all LOCAL pages, we can take advantage of React's Link component */}
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </div>
     )
 }
