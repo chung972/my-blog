373ce9eca391a209839521a1bcd19b76d672551c
Albert Chung <chung972@gmail.com>
2019-07-22 18:27:39 -0500

    imports and adds Link components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {Link} from "gatsby";
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About</h1>
             <p>I am a developer now</p>
+            <Link to="/contact">Contact me.</Link>
         </div>
     )
 }
