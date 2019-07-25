18d929f390ce9385b2dc0bbe9926fdddfeb6673c
Albert Chung <chung972@gmail.com>
2019-07-25 15:35:48 -0500

    imports and returns Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,15 @@
 import React from "react";
-import {Link} from "gatsby";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
 
 const AboutPage = () => {
     return (
-        <div>
+        <Layout>
             <h1>About</h1>
             <p>I am a developer now</p>
             <Link to="/contact">Contact me.</Link>
-        </div>
+        </Layout>
     )
 }
 
