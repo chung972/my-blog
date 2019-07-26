4c80d75860c29b260b3d7c37f6dab4d0c6c7e582
Albert Chung <chung972@gmail.com>
2019-07-26 09:43:57 -0500

    imports header.module.scss and uses classes; adds comments

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { Link } from "gatsby";
+import headerStyles from "./header.module.scss"
+// NOTE: just because you're IMPORTing a style sheet in a PARTICULAR component, 
+// that DOES NOT mean that those styles are SCOPED to that component
+
+// Also, BECAUSE these styles are GLOBALLY available (after compiling), even if we
+// were to give classNames to a specific component, if another component used the same
+// className, then we end up with styles from multiple files overriding each other;
+// before css modules, you could have long, complex class names, but that solution is 
+// not ideal; CSS MODULES make all of the CLASS SELECTORS in the module stylesheet
+// LOCALLY SCOPED, which means that we'll have to MANUALLY import and use them
 
 const Header = () => {
     return (
-        <header>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Albert Chung
+                </Link>
+            </h1>
             <nav>
-
-                <ul>
+                <ul className={headerStyles.navList}>
+                    {/* remember, css classes are camel cased in js */}
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/about">About</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
