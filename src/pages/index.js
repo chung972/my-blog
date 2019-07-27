c03f9b9845390df0c9b984f45710448e05a45f04
Albert Chung <chung972@gmail.com>
2019-07-27 11:04:34 -0500

    imports and returns a Head component; passing in title prop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import Head from "../components/head";
 
 const IndexPage = () => {
     return (
         <Layout>
+            <Head title="Home"/>
             <h1>Hello</h1>
             <h2>My name is Albert. A full-stack developer living in Texas.</h2>
             {/* <p>Need a developer? <a href="/contact">Contact me.</a></p> */}
