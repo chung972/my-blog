45b4f92465d757dac7235c6fd36810788d925322
Albert Chung <chung972@gmail.com>
2019-07-27 11:05:08 -0500

    installs and configures plugins for react-helmet and source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
     author: "Albert Chung"
   },
   plugins: [
-    "gatsby-plugin-sass"
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
   ]
 }
